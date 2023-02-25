a5a9b08bf6fdf563d68b3d86444daccc32d8f657
Parasite-231 <muktadir@iut-dhaka.edu>
2023-02-26 02:49:05 +0600

    user error page layout

--- mockui/src/App.js
+++ mockui/src/App.js
@@ -1,14 +1,20 @@
 // import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserErrorPage from "./pages//user/UserErrorPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline text-red-400">
-          Hello world!
-        </h1>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <h1 className="text-3xl font-bold underline text-red-400">
+    //       Hello world!
+    //     </h1>
+    //   </header>
+    // </div>
+    <Router>
+      <Routes>
+      <Route path="/" element={<UserErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
