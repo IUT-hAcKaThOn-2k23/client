fd90c54fee6be1beba298c187ac3b93a7da0ab76
Parasite-231 <muktadir@iut-dhaka.edu>
2023-02-26 17:05:13 +0600

    user login page

---
+++ mockui/src/components/user/userAuthenticationSection/UserLoginFormSideImage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import userLoginSideImage from "../../../assets/img/fooder.jpg"
+export default function UserLoginFormSideImage() {
+  return (
+    <div className="flex-1 bg-indigo-100 text-center  lg:flex ">
+    <div
+      className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+      
+    >
+
+        <img src={userLoginSideImage} alt=""/>
+    </div>
+   
+
+  </div>
+  )
+}
---
+++ mockui/src/components/user/userAuthenticationSection/UserSignUpRedirectionSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function UserSignUpRedirectionSection({redirectionHeadline,redirectionLink,redirectionText}) {
+  return (
+    <p className="mt-6 text-xs text-gray-600 text-center">
+   {redirectionHeadline}
+    <a href={redirectionLink} className="border-b border-gray-500 text-blue-600 border-dotted text-lg">
+      {redirectionText}
+    </a>
+   
+  </p>
+  )
+}
