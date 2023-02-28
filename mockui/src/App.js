56fbe473d3fa6593ecbc84e57e3341ac94d5c4e0
Parasite-231 <muktadir@iut-dhaka.edu>
2023-03-01 01:19:10 +0600

    ui test init

--- mockui/src/App.js
+++ mockui/src/App.js
@@ -1,5 +1,6 @@
 // import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import UserLoginFormLayout from "./components/user/userAuthenticationSection/UserLoginFormLayout";
 import UserErrorPage from "./pages//user/UserErrorPage";
 import UserEmailVerificationFormPage from "./pages/user/UserEmailVerificationFormPage";
 import UserLoginPage from "./pages/user/UserLoginPage";
@@ -13,6 +14,7 @@ function App() {
       <Routes>
       <Route path="/" element={<UserErrorPage />} />
       <Route path="/users/login" element={<UserLoginPage />} />
+
       <Route path="/users/signup" element={<UserSignUpPage />} />
       <Route path="/users/email-verification" element={<UserEmailVerificationFormPage />} />
       <Route path="/users/reset-password" element={<UserResetPasswordFormPage />} />
--- mockui/src/App.test.js
+++ mockui/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+// import AuthenticationFormHeaderText from "../src/components/user/userAuthenticationSection/AuthenticationFormHeaderText";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('header text test', () => {
+//   render(<AuthenticationFormHeaderText />);
+//   const linkElement = screen.getByText(/Login/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('testing all the layout components', () => {
+  
+  // eslint-disable-next-line testing-library/render-result-naming-convention
+  const component = render(<App />);
+  console.log(component)
 });
