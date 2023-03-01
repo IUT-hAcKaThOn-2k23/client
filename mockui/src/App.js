d2ab9c296e4ee3e5b481e197acf1796a8f039f6c
sakibesrat71 <esratebtida@iut-dhaka.edu>
2023-03-02 00:44:19 +0600

    Login and protected routing DONE

--- mockui/src/App.js
+++ mockui/src/App.js
@@ -8,17 +8,29 @@ import UserOTPVerificationFormPage from "./pages/user/UserOTPVerificationFormPag
 import UserResetPasswordFormPage from "./pages/user/UserResetPasswordFormPage";
 import UserSignUpPage from "./pages/user/UserSignUpPage";
 function App() {
+  var hours = 1; // to clear the localStorage after 1 hour
+  // (if someone want to clear after 8hrs simply change hours=8)
+  var now = new Date().getTime();
+  var setupTime = localStorage.getItem('setupTime');
+  if (setupTime == null) {
+    localStorage.setItem('setupTime', now)
+  } else {
+    if (now - setupTime > hours * 60 * 60 * 1000) {
+      localStorage.clear()
+      localStorage.setItem('setupTime', now);
+    }
+  }
   return (
 
     <Router>
       <Routes>
-      <Route path="/" element={<UserErrorPage />} />
-      <Route path="/users/login" element={<UserLoginPage />} />
+        <Route path="/" element={<UserErrorPage />} />
+        <Route path="/users/login" element={<UserLoginPage />} />
 
-      <Route path="/users/signup" element={<UserSignUpPage />} />
-      <Route path="/users/email-verification" element={<UserEmailVerificationFormPage />} />
-      <Route path="/users/reset-password" element={<UserResetPasswordFormPage />} />
-      <Route path="/users/OTP-verification" element={<UserOTPVerificationFormPage />} />
+        <Route path="/users/signup" element={<UserSignUpPage />} />
+        <Route path="/users/email-verification" element={<UserEmailVerificationFormPage />} />
+        <Route path="/users/reset-password" element={<UserResetPasswordFormPage />} />
+        <Route path="/users/OTP-verification" element={<UserOTPVerificationFormPage />} />
       </Routes>
     </Router>
   );
