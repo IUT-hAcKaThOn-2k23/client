8d2bf103930859e584f9e5fa5d116340c3f5a797
Parasite-231 <muktadir@iut-dhaka.edu>
2023-03-02 22:00:33 +0600

    last

--- mockui/src/components/user/userAuthenticationSection/HeaderCompanyLogo.js
+++ mockui/src/components/user/userAuthenticationSection/HeaderCompanyLogo.js
@@ -4,7 +4,7 @@ export default function HeaderCompanyLogo() {
   return (
     <div>
    
-    <h2 class="text-center">{"app logo"}</h2>
+    <h2 className="text-center">{"app logo"}</h2>
   </div>
   )
 }
